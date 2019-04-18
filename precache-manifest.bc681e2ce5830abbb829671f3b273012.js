22f8687cd8a4dc64cd12f142bad1ed78f0e21039
solang3 <solangegf@gmail.com>
2019-04-18 20:06:01 -0300

    Updates

---
+++ precache-manifest.bc681e2ce5830abbb829671f3b273012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53190b7a1b3cb99bb2c3",
+    "url": "/wp-login-react/static/js/runtime~main.fa12bf52.js"
+  },
+  {
+    "revision": "48985d54065d818b9cf0",
+    "url": "/wp-login-react/static/js/main.1dbecac4.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/wp-login-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "48985d54065d818b9cf0",
+    "url": "/wp-login-react/static/css/main.95b4e836.chunk.css"
+  },
+  {
+    "revision": "12f08bc6583488f4b48c818602bb1533",
+    "url": "/wp-login-react/index.html"
+  }
+];
