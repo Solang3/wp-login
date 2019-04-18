4f7626bac5bd714e2ceaf2ce7f55d28c4fea88eb
solang3 <solangegf@gmail.com>
2019-04-18 20:13:25 -0300

    A0001 - Setup

---
+++ build/precache-manifest.8657b9ea45b54e4ee7617834304f6f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53190b7a1b3cb99bb2c3",
+    "url": "/wp-login-react/static/js/runtime~main.fa12bf52.js"
+  },
+  {
+    "revision": "cbc4a60c839859d62f48",
+    "url": "/wp-login-react/static/js/main.79fc90dd.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/wp-login-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cbc4a60c839859d62f48",
+    "url": "/wp-login-react/static/css/main.95b4e836.chunk.css"
+  },
+  {
+    "revision": "719bdeb9322ca90cdba4c28722ffc275",
+    "url": "/wp-login-react/index.html"
+  }
+];
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Meeting Log</h1>
+        <h1>Login to WP</h1>
+        <p>A series of components for logging in and creating accounts on any wordpress site.</p>
       </div>
     );
   }
