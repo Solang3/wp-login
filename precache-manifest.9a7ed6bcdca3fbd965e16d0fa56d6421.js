29de161d02d1ea25103ab146542c42352e3abe3b
solang3 <solangegf@gmail.com>
2019-04-20 13:48:48 -0300

    Updates

--- precache-manifest.9a7ed6bcdca3fbd965e16d0fa56d6421.js
+++ precache-manifest.9a7ed6bcdca3fbd965e16d0fa56d6421.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "8ef6a08cdc1154920165680a4edde771",
-    "url": "/wp-login-react/static/media/arrow_right.8ef6a08c.svg"
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/wp-login-react/static/media/Roboto-Thin.954bbdeb.woff2"
   },
   {
-    "revision": "614638fcab9b517a5c4f",
-    "url": "/wp-login-react/static/css/main.aa5d2040.chunk.css"
+    "revision": "2ee2d4b79e560565e3fb",
+    "url": "/wp-login-react/static/css/main.6966961d.chunk.css"
   },
   {
     "revision": "53190b7a1b3cb99bb2c3",
     "url": "/wp-login-react/static/js/runtime~main.fa12bf52.js"
   },
   {
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
-    "url": "/wp-login-react/static/media/Roboto-Bold.e31fcf18.ttf"
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/wp-login-react/static/media/arrow_right.8ef6a08c.svg"
   },
   {
     "revision": "14d991a9f2ee939bd2d4",
     "url": "/wp-login-react/static/js/2.d398a821.chunk.js"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/wp-login-react/static/media/fa-brands-400.a7b95dbd.eot"
   },
   {
     "revision": "5e2f92123d241cabecf0b289b9b08d4a",
@@ -26,10 +30,6 @@ self.__precacheManifest = [
   {
     "revision": "2ef8ba3410dcc71578a880e7064acd7a",
     "url": "/wp-login-react/static/media/fa-brands-400.2ef8ba34.woff"
-  },
-  {
-    "revision": "a7b95dbdd87e0c809570affaf366a434",
-    "url": "/wp-login-react/static/media/fa-brands-400.a7b95dbd.eot"
   },
   {
     "revision": "98b6db59be947f563350d2284fc9ea36",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/wp-login-react/static/media/fa-regular-400.5eb754ab.svg"
   },
   {
-    "revision": "5bf145531213545e03ff41cd27df7d2b",
-    "url": "/wp-login-react/static/media/fa-brands-400.5bf14553.svg"
-  },
-  {
     "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
     "url": "/wp-login-react/static/media/fa-solid-900.46e7cec6.eot"
   },
   {
     "revision": "418dad87601f9c8abd0e5798c0dc1feb",
     "url": "/wp-login-react/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/wp-login-react/static/media/fa-brands-400.5bf14553.svg"
   },
   {
     "revision": "a7140145ebaaf5fb14e40430af5d25c4",
@@ -80,40 +80,32 @@ self.__precacheManifest = [
     "url": "/wp-login-react/static/media/fa-solid-900.49279363.svg"
   },
   {
-    "revision": "11911410dca2de148f30954eb2fd5eab",
-    "url": "/wp-login-react/static/media/arrow_left.11911410.svg"
-  },
-  {
-    "revision": "614638fcab9b517a5c4f",
-    "url": "/wp-login-react/static/js/main.8395bf03.chunk.js"
-  },
-  {
     "revision": "dfe56a876d0282555d1e2458e278060f",
     "url": "/wp-login-react/static/media/Roboto-Thin.dfe56a87.eot"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/wp-login-react/static/media/Roboto-Thin.94998475.ttf"
-  },
-  {
-    "revision": "a990f611f2305dc12965f186c2ef2690",
-    "url": "/wp-login-react/static/media/Roboto-Light.a990f611.eot"
+    "revision": "2ee2d4b79e560565e3fb",
+    "url": "/wp-login-react/static/js/main.3d272620.chunk.js"
   },
   {
     "revision": "7500519de3d82e33d1587f8042e2afcb",
     "url": "/wp-login-react/static/media/Roboto-Thin.7500519d.woff"
   },
   {
-    "revision": "954bbdeb86483e4ffea00c4591530ece",
-    "url": "/wp-login-react/static/media/Roboto-Thin.954bbdeb.woff2"
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/wp-login-react/static/media/Roboto-Light.a990f611.eot"
   },
   {
-    "revision": "3b813c2ae0d04909a33a18d792912ee7",
-    "url": "/wp-login-react/static/media/Roboto-Light.3b813c2a.woff"
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/wp-login-react/static/media/Roboto-Thin.94998475.ttf"
   },
   {
     "revision": "69f8a0617ac472f78e45841323a3df9e",
     "url": "/wp-login-react/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/wp-login-react/static/media/Roboto-Light.3b813c2a.woff"
   },
   {
     "revision": "46e48ce0628835f68a7369d0254e4283",
@@ -132,12 +124,12 @@ self.__precacheManifest = [
     "url": "/wp-login-react/static/media/Roboto-Regular.ba3dcd89.woff"
   },
   {
-    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
-    "url": "/wp-login-react/static/media/Roboto-Medium.4d9f3f9e.eot"
-  },
-  {
     "revision": "df7b648ce5356ea1ebce435b3459fd60",
     "url": "/wp-login-react/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/wp-login-react/static/media/Roboto-Medium.4d9f3f9e.eot"
   },
   {
     "revision": "574fd0b50367f886d359e8264938fc37",
@@ -164,11 +156,19 @@ self.__precacheManifest = [
     "url": "/wp-login-react/static/media/Roboto-Bold.dc81817d.woff"
   },
   {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/wp-login-react/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/wp-login-react/static/media/arrow_left.11911410.svg"
+  },
+  {
     "revision": "14d991a9f2ee939bd2d4",
     "url": "/wp-login-react/static/css/2.fb8b118e.chunk.css"
   },
   {
-    "revision": "be8fa32c6faf471289ba840788b909f1",
+    "revision": "e22683d91aee934bc98526e660bb8f4e",
     "url": "/wp-login-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wp-login-react/precache-manifest.3739c97e1fdb7d6e5f562c28b865c997.js"
+  "/wp-login-react/precache-manifest.9a7ed6bcdca3fbd965e16d0fa56d6421.js"
 );
 
 workbox.clientsClaim();
