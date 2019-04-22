c39eb71da38cfa5995b88341a2e894ae59e40640
solang3 <solangegf@gmail.com>
2019-04-22 17:40:42 -0300

    Login

--- src/components/Authenticate/LogIn.js
+++ src/components/Authenticate/LogIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
 const LogInForm = () => {
+  
   return (
     <MDBContainer className="formContainer">
       <MDBRow>
@@ -16,6 +17,7 @@ const LogInForm = () => {
                 validate
                 error="wrong"
                 success="right"
+                name="user_login"
               />
               <MDBInput
                 label="Type your password"
@@ -23,10 +25,11 @@ const LogInForm = () => {
                 group
                 type="password"
                 validate
+                name="password"
               />
             </div>
             <div className="text-center">
-                <MDBBtn>Login</MDBBtn>
+                <MDBBtn onClick={doLogin()}>Login</MDBBtn>
                 <p className="font-small d-flex justify-content-center pt-3 mt-3 pb-3">
                   Forgot
                   <a href="#!" className="ml-1">
